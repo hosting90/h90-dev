cd1888fce848f7608bc3b953f47c44383ae8e532
mdivecky <matej@divecky.com>
2019-04-11 09:11:32 +0200

    (more) sensible variables, code moved to function, working output

--- wildcard_automatizace/wcgen.py
+++ wildcard_automatizace/wcgen.py
@@ -3,10 +3,12 @@ import logging
 import os,sys
 import pkg_resources
 import re
+import json
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography import x509
 import josepy as jose
 import OpenSSL
 
@@ -27,6 +29,99 @@ REG_DIRECTORY = 'registrations'
 
 
 
+def wildcard_request(cn, account):
+    #Check if CN is valid domain
+    domain_regex = re.compile("^([a-zA-Z0-9]([\-a-zA-Z0-9]{0,61}[a-zA-Z0-9])?\.)*([a-zA-Z0-9]([\-a-zA-Z0-9]{0,61}[a-zA-Z0-9])+\.)([a-zA-Z0-9]+([\-a-zA-Z0-9]{0,61}[a-zA-Z])+)$")
+    if not domain_regex.match(cn):
+        print 'First argument is not valid CN'
+        sys.exit(1)
+
+    #Check if registrar exists
+    if account not in os.listdir(REG_DIRECTORY):
+    	print "This account does not exists, register it first with new_account.py"
+    	sys.exit(1)
+
+    #Load files from disk
+    with open(REG_DIRECTORY + "/" + account + "/private.key", "rb") as key_file:
+        privkey = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
+    with open(REG_DIRECTORY + "/" + account + "/reguri.txt", "r") as reguri_file:
+    	reg_uri = reguri_file.read()
+
+
+    #Compose regr
+    key = jose.JWKRSA(key=privkey)
+    regr = messages.RegistrationResource(
+    	body=messages.Registration(
+    		key=key.public_key()),
+    	uri = reg_uri)
+
+    #Init ACME
+    net = ClientNetwork(key)
+    directory = net.get(DIRECTORY_URL).json()
+    acme = client.ClientV2(directory, net)
+
+    #Check if registration is valid
+    if acme.query_registration(regr).body.status == u'valid':
+    	print "Registration valid"
+    else:
+    	print "Registration invalid"
+    	sys.exit(1)
+
+    #Generate private key for certificate
+    pkey = OpenSSL.crypto.PKey()
+    pkey.generate_key(OpenSSL.crypto.TYPE_RSA, BITS)
+
+    #Serialize key for output
+    pkey_printable = OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, pkey, cipher=None, passphrase=None)
+
+    #Compose request for acme
+    req = crypto_util.make_csr(OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM,pkey),[cn,'*.'+cn])
+
+    #begin order
+    orderr = acme.new_order(req)
+    d = ''
+    while d.strip() == '':
+    	for authr in orderr.authorizations:
+    		for chalr in authr.body.challenges:
+    			if type(chalr.chall) == type(challenges.DNS01()):
+    				print "%s %s" % (chalr.chall.validation_domain_name(cn),chalr.chall.validation(key))
+    				#logging.debug(orderr.authorizations[0].body.challenges[0].chall)
+    				# jedem DNS validaci
+    	d = sys.stdin.readline()
+    	for authr in orderr.authorizations:
+    		for chalr in authr.body.challenges:
+    			if type(chalr.chall) == type(challenges.DNS01()):
+    				try:
+    					acme.answer_challenge(chalr,challenges.DNS01Response())
+    				except:
+    					print chalr.chall.encode('token')+" already answered"
+
+    #After filling DNS and waiting for propagation, finalize order
+    res = acme.poll_and_finalize(orderr)
+
+    #logging.info(res)
+
+    cert = x509.load_pem_x509_certificate(str(res.fullchain_pem), default_backend())
+
+
+
+    output_data = {
+        'wildcard': {
+            'cn' : cn,
+            'private_key' : str(pkey_printable),
+            'certificate' : str(res.fullchain_pem),
+            'expiration' : cert.not_valid_after.strftime("%x %X") #Locale-specific time+date representation. Edit to your need
+        }
+    }
+
+
+    print json.dumps(output_data)
+
+# DEBUG INPUT FROM TERMINAL
 def usage():
     print 'Usage: ' + sys.argv[0] + ' CN REG '
     print 'example: ' + sys.argv[0] + ' moje-domena.cz h90'
@@ -36,103 +131,10 @@ def input_san():
     if len(sys.argv) != 3 :
         usage()
     else:
-        global DOMAIN, registrar
-        DOMAIN = sys.argv[1]
-        registrar = sys.argv[2]
-
-	#Check if CN is valid domain
-    domain_regex = re.compile("^([a-zA-Z0-9]([\-a-zA-Z0-9]{0,61}[a-zA-Z0-9])?\.)*([a-zA-Z0-9]([\-a-zA-Z0-9]{0,61}[a-zA-Z0-9])+\.)([a-zA-Z0-9]+([\-a-zA-Z0-9]{0,61}[a-zA-Z])+)$")
-    if not domain_regex.match(DOMAIN):
-        print 'First argument is not valid CN'
-        sys.exit(1)
-
-	#Check if registrar exists
-	if registrar not in os.listdir(REG_DIRECTORY):
-		print "This registrar does not exists, register it first with new_registrator.py"
-		sys.exit(1)
+        cn = sys.argv[1]
+        account = sys.argv[2]
+        wildcard_request(cn, account)
 
 
-
-def load_files():
-	global privkey, reg_uri
-	with open(REG_DIRECTORY + "/" + registrar + "/private.key", "rb") as key_file:
-	    privkey = serialization.load_pem_private_key(
-	        key_file.read(),
-	        password=None,
-	        backend=default_backend()
-	    )
-	with open(REG_DIRECTORY + "/" + registrar + "/reguri.txt", "r") as reguri_file:
-		reg_uri = reguri_file.read()
-
-
-def compose_regr():
-	global regr, key
-	key = jose.JWKRSA(key=privkey)
-	regr = messages.RegistrationResource(
-		body=messages.Registration(
-			key=key.public_key()),
-		uri = reg_uri)
-
-
-
-
-def acme_query():
-
-	net = ClientNetwork(key)
-	directory = net.get(DIRECTORY_URL).json()
-	acme = client.ClientV2(directory, net)
-
-	#Check if registration is valid
-	if acme.query_registration(regr).body.status == u'valid':
-		print "Registration valid"
-	else:
-		print "Registration invalid"
-		sys.exit(1)
-
-
-	# csr = OpenSSL.crypto.load_certificate_request(
-	# 	OpenSSL.crypto.FILETYPE_ASN1, pkg_resources.resource_string(
-	# 		'acme', os.path.join('testdata', 'csr.der')))
-
-	pkey = OpenSSL.crypto.PKey()
-	pkey.generate_key(OpenSSL.crypto.TYPE_RSA, BITS)
-
-	req = crypto_util.make_csr(OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM,pkey),[DOMAIN,'*.'+DOMAIN])
-
-
-	orderr = acme.new_order(req)
-	d = ''
-	while d.strip() == '':
-		for authr in orderr.authorizations:
-			for chalr in authr.body.challenges:
-				if type(chalr.chall) == type(challenges.DNS01()):
-					print "%s %s" % (chalr.chall.validation_domain_name(DOMAIN),chalr.chall.validation(key))
-					#logging.debug(orderr.authorizations[0].body.challenges[0].chall)
-					# jedem DNS validaci
-		d = sys.stdin.readline()
-		for authr in orderr.authorizations:
-			for chalr in authr.body.challenges:
-				if type(chalr.chall) == type(challenges.DNS01()):
-					try:
-						acme.answer_challenge(chalr,challenges.DNS01Response())
-					except:
-						print chalr.chall.encode('token')+" already answered"
-
-	res = acme.poll_and_finalize(orderr)
-	print "PRIVKEY:"
-	print OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, pkey, cipher=None, passphrase=None)
-	print "FULLCHAIN:"
-	print(res.fullchain_pem)
-
-
-
-
-
-
-def main():
-	input_san()
-	load_files()
-	compose_regr()
-	acme_query()
-
-main()
+input_san() #Input from arguments
+wildcard_request("divecky.com", "h90") #Function input
