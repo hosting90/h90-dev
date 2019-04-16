5cbaff11eba311e45cc29c4d770bc7b0d3a7cbf9
mdivecky <matej@divecky.com>
2019-04-16 17:03:00 +0200

    celkem naslepo dopsany compile zony

--- dns_write.py
+++ dns_write.py
@@ -1,64 +1,125 @@
 """ DNS WRITE """
+#from fnctl import flock, LOCK_EX, LOCK_NB, LOCK_SH, LOCK_UN
+import os
+import re
+import datetime
+import base
+import mydns #Better specify just sign_and_compile?
 
 
-
-
-DNSPATH = 'master'
-# cn = "divecky.com"
-# validation_data = [[u'a3k34_87XB13BwGQKk6yLpbMBgy451m-Sa32ZcloyMw', '_acme-challenge.adivecky.com'], [u'9SvLl7qboD6d6cPuxHG7Va5jPt_MsX_BIRI7NgoAF9g', '_acme-challenge.adivecky.com']]
+DNS_MASTER_ZONE_PATH='/var/named/master'
+DNS_COMPILED_ZONE_PATH='/var/named/master-compiled'
 
 #Main functions
 def dns_apply_challenge(cn, validation_data):
-    if dns_challenge_in_file(cn):
-        dns_remove_challenge(cn)
-    dns_write_to_file(cn, validation_data)
+    base_domain = extract_base_domain(cn)
+    zonefile = DNS_MASTER_ZONE_PATH + "/" + base_domain
+    zonefile_compiled = DNS_COMPILED_ZONE_PATH + "/" + base_domain
 
-def dns_remove_challenge(cn):
-    zonefile = give_me_zonefile(cn)
-    with open(DNSPATH + "/" + zonefile, "r+") as f:
+    #Check if there are any old records. If so, remove them.
+    if dns_challenge_in_file(zonefile):
+        dns_remove_challenge(zonefile, False)
+
+    #Write challenges to zonefile
+    dns_recs = []
+    for data in validation_data:
+        dns_recs.append('%s\t60\tIN\tTXT\t"%s"\n' % (data[1].rsplit(".",2)[:-2][0], data[0]))
+    zonefile_append = open(zonefile, "a+")
+    for dns_rec in dns_recs:
+        zonefile_append.write(dns_rec)
+    zonefile_append.close()
+    #Check if we really have chalres in zonefile
+    if dns_challenge_in_file(zonefile):
+        dns_compile_zonefile(base_domain, zonefile, zonefile_compiled)
+    else:
+        print "Unable to write challenge"
+        return False
+
+def dns_remove_challenge(cn, commit = True):
+    base_domain = extract_base_domain(cn)
+    zonefile = DNS_MASTER_ZONE_PATH + "/" + base_domain
+    zonefile_compiled = DNS_COMPILED_ZONE_PATH + "/" + base_domain
+    with open(DNS_MASTER_ZONE_PATH + "/" + base_domain, "r+") as f:
         d = f.readlines()
         f.seek(0)
         for i in d:
             if "_acme-challenge" not in i:
                 f.write(i)
         f.truncate()
-    if dns_challenge_in_file(cn):
+    if dns_challenge_in_file(zonefile):
         print "Something is wrong, cannot remove challenge"
         return False
 
+    #If commit variable is False, do not compile&commit to bind. Just remove records from fileself.
+    #Designed for removing residual challenges before trying new
+    if not commit:
+        return True
+    else:
+        #Start commiting process.
+        print ""
+
 #Subfunctions
-def dns_challenge_in_file(cn):
-    zonefile = give_me_zonefile(cn)
+def extract_base_domain(cn):
+    #Just removing everything except top and second level domains (eg. new.docs.hosting90.cz => hosting90.cz)
+    nsfile = cn.split(".")[-2:]
+    base_domain = nsfile[0] + "." + nsfile[1]
+    return base_domain
+def dns_challenge_in_file(zonefile):
     list = []
-    for line in open(DNSPATH + "/" + zonefile, "r"):
+    for line in open(zonefile, "r"):
         if "_acme-challenge" in line:
             list.append(line)
     if list:
         return True
+        #return list
     else:
         return False
 
 
-def dns_write_to_file(cn, validation_data):
-    zonefile = give_me_zonefile(cn)
-    dns_recs = []
-    for data in validation_data:
-        dns_recs.append('%s\t60\tIN\tTXT\t"%s"\n' % (data[1].rsplit(".",2)[:-2][0], data[0]))
-    dnsfile = open(DNSPATH + "/" + zonefile, "a+")
-    for dns_rec in dns_recs:
-        dnsfile.write(dns_rec)
-    dnsfile.close()
-    if dns_challenge_in_file(cn):
+def dns_detect_dnssec(zonefile_compiled):
+    if 'DNSKEY' in open(zonefile_compiled).read():
         return True
     else:
-        print "Unable to write challenge"
         return False
 
-#dns_apply_challenge(cn, validation_data)
-#dns_remove_challenge(cn)
+def dns_compile_zonefile(base_domain, zonefile, zonefile_compiled):
+    if dns_detect_dnssec(zonefile_compiled):
+        print "" #Here, we just use prepared function from mydns.py
+        dnssec_write = mydns.sign_and_compile(base_domain, zonefile, zonefile_compiled, True)
+        return dnssec_write
 
-#Just removing everything except top and second level domains (eg. new.docs.hosting90.cz => hosting90.cz)
-def give_me_zonefile(cn):
-    nsfile = cn.split(".")[-2:]
-    zonefile = nsfile[0] + "." + nsfile[1]
-    return zonefile
+    increment_serial = True
+    serial_updated = False
+    line = None
+    srcfh = open(zonefile,'r+')
+    while line == None or line != '':
+        line = srcfh.readline()
+        if increment_serial and not serial_updated:
+            mymatch = re.match('^(\s+([0-9]{10})\s*;\s*serial\s*)$',line)
+            if mymatch != None:
+                old_serial = int(mymatch.group(2))
+                today_serial = int(datetime.date.today().strftime('%Y%m%d00'))
+                new_serial = max(old_serial,today_serial)+1
+                line = line.replace(mymatch.group(2),str(new_serial))
+                filepos = srcfh.tell()
+                srcfh.seek(filepos-len(line))
+                srcfh.write(line)
+                serial_updated=True
+    srcfh.close()
+
+    #Compile zone
+    (out, err, res) = base.shell_exec2('named-compilezone -o '+zonefile_compiled+' '+ base_domain +' '+zonefile)
+    compile_result = res == 0
+
+
+    os.utime(zonefile_compiled, None)
+
+
+    if compile_result == True:
+        base.shell_exec('rndc reload '+base_domain)
+        return [True, "ok" ]
+    else:
+        return [True, "Zone not loaded:\n"+str(out)+"\n\n"+str(err)]
+
+#debug run
+dns_compile_zonefile("divecky.com", "master/divecky.com", "master-compiled/divecky.com")
